88a2fe04b3f594de8af9a329f142038923103e69
Brian Azizi <briana@theodo.co.uk>
2021-01-25 12:43:11 +0000

    Draw projectile to ppm file

--- examples/projectile.py
+++ examples/projectile.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass
 from src.tuple import Point, Vector, point, vector, normalize
+from src.color import Color
+from src.canvas import Canvas
+from src.ppm import PPM
 
 
 @dataclass
@@ -20,19 +23,36 @@ def tick(environment: Environment, projectile: Projectile) -> Projectile:
     return Projectile(position, velocity)
 
 
+def draw_projectile(canvas: Canvas, projectile: Projectile) -> None:
+    projectile_color = Color(1, 0, 0)
+    x = int(round(projectile.position.x))
+    y = canvas.height - int(round(projectile.position.y))
+
+    canvas.write_pixel(x, y, projectile_color)
+
+
 def run() -> None:
-    projectile = Projectile(
-        point(0, 1, 0),
-        normalize(vector(1, 1, 0)),
-    )
-    environment = Environment(
-        vector(0, -0.1, 0),
-        vector(-0.01, 0, 0),
-    )
+    start = point(0, 1, 0)
+    velocity = normalize(vector(1, 1, 0))
+    gravity = vector(0, -0.1, 0)
+    wind = vector(-0.01, 0, 0)
+
+    projectile = Projectile(start, velocity)
+    environment = Environment(gravity, wind)
+    canvas = Canvas(10, 10)
+
+    draw_projectile(canvas, projectile)
     num_ticks = 0
     while projectile.position.y >= 0:
         projectile = tick(environment, projectile)
+        draw_projectile(canvas, projectile)
         num_ticks += 1
 
-    print(projectile)
+    ppm = PPM(canvas)
+
+    print(ppm.to_string())
     print(f"Projectile flew for {num_ticks} ticks.")
+
+    file_name = "projectile.ppm"
+    ppm.save_to_file(file_name)
+    print(f"Output stored to {file_name}")
