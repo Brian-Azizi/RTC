24e0c0834cf8be8c19afc5958ed8131805dee5a1
Brian Azizi <briana@theodo.co.uk>
2021-01-26 12:55:30 +0000

    Add scaling

--- features/steps/transformations.py
+++ features/steps/transformations.py
@@ -1,7 +1,12 @@
 from behave import given, then
-from src.transformations import translation
+from src.transformations import translation, scaling
 
 
 @given("{var:w} ← translation({x:g}, {y:g}, {z:g})")
 def assign_translation(context, var, x, y, z):
     context.variables[var] = translation(x, y, z)
+
+
+@given("{var:w} ← scaling({x:g}, {y:g}, {z:g})")
+def assign_scaling(context, var, x, y, z):
+    context.variables[var] = scaling(x, y, z)
--- src/transformations.py
+++ src/transformations.py
@@ -7,3 +7,11 @@ def translation(x: float, y: float, z: float) -> Matrix:
     result[1, 3] = y
     result[2, 3] = z
     return result
+
+
+def scaling(x: float, y: float, z: float) -> Matrix:
+    result = identity(4)
+    result[0, 0] = x
+    result[1, 1] = y
+    result[2, 2] = z
+    return result
