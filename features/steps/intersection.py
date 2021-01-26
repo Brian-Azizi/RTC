a1e3dfb90b6f7eca9f3f59a1e37d7a78e21d3749
Brian Azizi <briana@theodo.co.uk>
2021-01-26 17:56:19 +0000

    Implement set of intersections

--- features/steps/intersection.py
+++ features/steps/intersection.py
@@ -1,8 +1,22 @@
 from behave import when, given, then
-from src.intersection import Intersection
+from src.intersection import Intersection, intersections
 
 
+@given(u"{var:w} ← intersection({t:g}, {obj:w})")
 @when(u"{var:w} ← intersection({t:g}, {obj:w})")
 def assign_intersection(context, var, t, obj):
     variable = context.variables[obj]
     context.variables[var] = Intersection(t, variable)
+
+
+@when(u"{var:w} ← intersections({var_list})")
+def assign_intersections(context, var, var_list):
+    var_names = [v.strip() for v in var_list.split(",")]
+    variables = [context.variables[v] for v in var_names]
+    context.variables[var] = intersections(*variables)
+
+
+@then(u"{var:w}[{index:d}].{attribute:w} = {value:g}")
+def check_list_member_attribute(context, var, index, attribute, value):
+    array = context.variables[var]
+    assert getattr(array[index], attribute) == value
--- src/intersection.py
+++ src/intersection.py
@@ -1,3 +1,4 @@
+from typing import List, Iterable
 from src.spheres import Sphere
 
 
@@ -8,3 +9,7 @@ class Intersection:
     def __init__(self, t: float, the_object: Sphere):
         self.t = t
         self.the_object = the_object
+
+
+def intersections(*args: Intersection) -> List[Intersection]:
+    return list(args)
