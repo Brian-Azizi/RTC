a093029bae4a01b5fa139540c44dbf6a6cfa0be1
Brian Azizi <briana@theodo.co.uk>
2021-01-27 14:56:42 +0000

    calculate normal of spheres

--- features/steps/spheres.py
+++ features/steps/spheres.py
@@ -1,6 +1,9 @@
 from behave import given, then, when
-from src.spheres import sphere, intersect
-from src.transformations import scaling, translation
+from typing import Callable, List
+from src.spheres import sphere, intersect, normal_at
+from src.transformations import scaling, translation, rotation_z
+from src.tuple import normalize, point
+from src.matrix import Matrix
 
 
 @given("{var:w} ← sphere()")
@@ -22,22 +25,51 @@ def check_index(context, var, index, value):
 
 
 @when("set_transform({sphere_object:w}, {transform:w})")
+@given("set_transform({sphere_object:w}, {transform:w})")
 def set_transform_1(context, sphere_object, transform):
     s = context.variables[sphere_object]
     t = context.variables[transform]
     s.set_transform(t)
 
 
+def parse_transform(transform_string: str, args_string: str) -> Matrix:
+    if transform_string == "scaling":
+        transformation = scaling  # type: Callable[..., Matrix]
+    elif transform_string == "translation":
+        transformation = translation
+    elif transform_string == "rotation_z":
+        transformation = rotation_z
+    else:
+        raise ValueError(f"Transformation function {transform_string} not recognized")
+
+    arguments = [float(x) for x in args_string.split(",")]
+    return transformation(*arguments)
+
+
 @when("set_transform({sphere_object:w}, {transform:w}({args}))")
+@given("set_transform({sphere_object:w}, {transform:w}({args}))")
 def set_transform_2(context, sphere_object, transform, args):
     s = context.variables[sphere_object]
-    if transform == "scaling":
-        transformation = scaling
-    elif transform == "translation":
-        transformation = translation
-    else:
-        raise ValueError(f"Transformation function {transform} not recognized")
+    t = parse_transform(transform, args)
 
-    arguments = [float(x) for x in args.split(",")]
-    t = transformation(*arguments)
     s.set_transform(t)
+
+
+@given("{var:w} ← {transform_1:w}({args_1}) * {transform_2:w}({args_2})")
+def assign_transformation(context, var, transform_1, args_1, transform_2, args_2):
+    t_1 = parse_transform(transform_1, args_1)
+    t_2 = parse_transform(transform_2, args_2)
+    context.variables[var] = t_1 * t_2
+
+
+@when("{var:w} ← normal_at({obj_var:w}, point({x:g}, {y:g}, {z:g}))")
+def assign_normal(context, var, obj_var, x, y, z):
+    the_object = context.variables[obj_var]
+    context.variables[var] = normal_at(the_object, point(x, y, z))
+
+
+@then("{var_1:w} = normalize({var_2:w})")
+def check_normal(context, var_1, var_2):
+    variable_1 = context.variables[var_1]
+    variable_2 = context.variables[var_2]
+    assert variable_1 == normalize(variable_2)
--- src/spheres.py
+++ src/spheres.py
@@ -1,9 +1,9 @@
 import math
 from typing import List
-from src.tuple import point, Point, dot
+from src.tuple import point, Point, dot, normalize, Vector, vector
 from src.rays import Ray, transform
 from src.intersection import Intersection, intersections, Intersections, Object
-from src.matrix import identity, Matrix, inverse
+from src.matrix import identity, Matrix, inverse, transpose
 
 
 class Sphere(Object):
@@ -44,3 +44,12 @@ def intersect(s: Sphere, ray: Ray) -> Intersections:
         Intersection((-b - math.sqrt(discriminant)) / (2 * a), s),
         Intersection((-b + math.sqrt(discriminant)) / (2 * a), s),
     )
+
+
+def normal_at(s: Sphere, world_point: Point) -> Vector:
+    transformer = inverse(s.transform)
+    object_point = transformer * world_point
+    object_normal = object_point - point(0, 0, 0)
+    world_normal = transpose(transformer) * object_normal
+    world_normal = vector(world_normal.x, world_normal.y, world_normal.z)  # set w to 0
+    return normalize(world_normal)
